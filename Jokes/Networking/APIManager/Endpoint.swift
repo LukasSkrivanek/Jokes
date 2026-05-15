53f740345a73fab69fc72c743f4c9412ed36ad53
Lukas Skrivanek <Lukas.Skrivanek@adastragrp.com>
2026-05-15 09:34:04 +0200

    feat: Endpoint protocol

---
+++ Jokes/Networking/APIManager/Endpoint.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+protocol Endpoint {
+    var host: URL { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String: String] { get }
+    var urlParameters: [String: String] { get }
+
+    func asURLRequest() throws -> URLRequest
+}
